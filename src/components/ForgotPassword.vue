f5f85b2b4178a0d44dbc4d2f2eb11df26f0b2245
Grace Vivian <gracevivian04@gmail.com>
2023-02-06 13:46:43 +0100

    password recovery added

---
+++ src/components/ForgotPassword.vue
@@ -0,0 +1,40 @@
+<!--need to add router-link to front login page-->
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Forgot Password</h3>
+    <p>Please enter the email address associated to your Flitter account and we'll send you a link to reset your password</p>
+    <div class="form-group">
+      <label>Email</label>
+      <input type="email" class="form-control" v-model="email" placeholder="Email" />
+    </div>
+    <button class="btn btn-primary btn block">Continue</button>
+    <p>Don't have an account?</p><router-link to="/register">Sign Up</router-link>
+  </form>
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from 'axios';
+
+export default defineComponent({
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      const response = await axios.post('forgotPassword', {
+        email: this.email
+      });
+
+      console.log(response)
+    }
+  }
+})
+
+</script>
---
+++ src/components/ResetPassword.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Reset Password</h3>
+    <div class="form-group">
+      <label>New Password</label>
+      <input type="password" class="form-control" v-model="password" placeholder="Password" />
+    </div>
+    <div class="form-group">
+      <label>Confirm Password</label>
+      <input type="password" class="form-control" v-model="confirmed_password" placeholder="Confirm Password" />
+    </div>
+    <button class="btn btn-primary btn-block">Enter</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'ResetPassword',
+  data() {
+    return {
+      password: '',
+      confirmed_password: '',
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      const response = await axios.post('resetPassword', {
+          password: this.password,
+          confirmed_password: this.confirmed_password
+          token: this.$route.params.token
+      });
+
+      console.log(response);
+      this.$router.push('/login');
+    }
+  }
+
+})
+
+</script>
