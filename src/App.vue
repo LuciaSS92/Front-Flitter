4800102bed69e1a05aac766b51da55dc54a5b842
LuciaSaenz <luciasaenz92@hotmail.com>
2023-02-07 18:03:40 +0100

    signup component OK

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>|
-    <router-link to="/feed">Feed</router-link>
+    <router-link to="/feed">Feed</router-link> |
+    <router-link to="/signup">Sign Up</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,14 @@
+<template><SignUp /></template>
+
+<script lang="ts" >
+import SignUp from '@/components/SignUp.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent( {
+    name: 'SignUpView',
+    components: {
+        SignUp
+    },
+})
+
+</script>
